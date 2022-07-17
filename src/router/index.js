cf8358f46ab2e603599445b55a07ce9d4f298c67
Luke Hyoungwon Kang <hwplus@gmail.com>
2022-07-18 00:40:06 +0900

    07.17

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import DataBinding from '../views/DataBinding.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+// import DataBinding from "../views/DataBinding.vue";
+// import DataBindingHtml from "../views/DataBindingHtml.vue";
+// import DataBindingInputText from "../views/DataBindingInputText.vue";
+// import DataBindingInputNumber from "../views/DataBindingInputNumber.vue";
+// import DataBindingSelect from "../views/DataBindingSelect.vue";
+// import DataBindingCheckbox from "../views/DataBindingCheckbox.vue";
+// import DataBindingCheckbox2 from "../views/DataBindingCheckbox2.vue";
+import DataBindingRadio from "../views/DataBindingRadio.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackPrefetch: true */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackPrefetch: true */ "../views/AboutView.vue"),
   },
   {
-    path: '/databinding',
-    name: 'DataBinding',
-    component: DataBinding
-  }
-]
+    path: "/databinding",
+    name: "DataBinding",
+    component: DataBindingRadio,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;

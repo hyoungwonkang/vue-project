82d01e5bca09a296edc1a1ec952523c175089a55
Luke Hyoungwon Kang <hwplus@gmail.com>
2022-07-17 21:32:41 +0900

    DataBindingHtml

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/databinding">DataBinding</router-link>
+  </div>
+  <router-view />
 </template>
 
 <style>
